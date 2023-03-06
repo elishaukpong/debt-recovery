74e7076874e483744fa717ce0c66e90753ff7b86
Favizille <89010415+Favizille@users.noreply.github.com>
2023-03-06 23:37:41 +0100

    user authentication completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignInRequest;
+use App\Http\Requests\SignUpRequest;
+use App\Repository\Eloquent\AuthRepository;
 
 class AuthController extends Controller
 {
+    protected $authRepository;
+
+    public function __construct( AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function responseIsTrue(){
+        return true;
+    }
+
+    public function responseIsFalse(){
+        return false;
+    }
+
     public function signInPage(){
         return view('Authentication.signin');
     }
 
     public function signUpPage(){
         return view("Authentication.signup");
+    }
+
+    public function signUp(SignUpRequest $request){
+        $signUpResponse = $this->authRepository->signUp($request->validated());
+
+
+        if($signUpResponse == $this->responseIsFalse()) {
+            return view('register', ["message" => $signUpResponse['message']]);
+        }
+
+        return redirect("/dashboard");
+
+    }
+
+    public function signIn(SignInRequest $request){
+        $signInResponse = $this->authRepository->signIn($request->validated());
+
+        if($signInResponse == $this->responseIsFalse()) {
+            return view('Authentication.signup', ["message" => $signInResponse['message']]);
+        }
+
+        return redirect("/dashboard");
     }
 }
