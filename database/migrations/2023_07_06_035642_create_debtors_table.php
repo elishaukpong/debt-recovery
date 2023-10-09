4d8c914fe9ddc43b23beb398a37bc77434ab26ec
unknown <favourgabriel50@gmail.com>
2023-10-09 21:36:34 +0100

    working on schedule for reminder

--- database/migrations/2023_07_06_035642_create_debtors_table.php
+++ database/migrations/2023_07_06_035642_create_debtors_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('debtors', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id");
-            $table->foreignId("reminder");
+            $table->foreignId("reminder")->nullable();
             $table->string("first_name");
             $table->string("last_name");
             $table->string("email");
